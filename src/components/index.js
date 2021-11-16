c4a3e3e224e620a1a446debc5296cfba4737c53c
trungducng <nguyentrungduc2910@gmail.com>
2021-11-16 23:25:48 +0700

    add cryptocurrencies page

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Homepage from "./Homepage";
-import News from "./Homepage";
+import News from "./News";
 import Exchanges from "./Exchanges";
 import CryptoDetails from "./CryptoDetails";
 import Cryptocurrencies from "./Cryptocurrencies";
